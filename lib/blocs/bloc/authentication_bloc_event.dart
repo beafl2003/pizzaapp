9bd9a0d550f174155b3fb124587584ac0caa7186
Anna Beatriz Azevedo Fidelis <109418073+beafl2003@users.noreply.github.com>
2024-07-07 00:47:45 -0300

    Initial

---
+++ lib/blocs/bloc/authentication_bloc_event.dart
@@ -0,0 +1,8 @@
+part of 'authentication_bloc_bloc.dart';
+
+sealed class AuthenticationBlocEvent extends Equatable {
+  const AuthenticationBlocEvent();
+
+  @override
+  List<Object> get props => [];
+}
---
+++ packages/user_repository/lib/src/user_repo.dart
@@ -0,0 +1,16 @@
+import 'models/models.dart';
+
+abstract class UserRepository {
+  Stream<MyUser?> get user;
+
+  Future<MyUser> signUp(MyUser myUser, String password);
+
+  Future<void> setUserData(MyUser user);
+
+  Future<void> signIn(String email, String password);
+
+  Future<void> logOut();
+}
+
+// checking the authentication of the user 
+// we will implement the methods in the firebase_user_repo
